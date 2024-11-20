28bb98cd71cb8dd3d403b2c940b7de5c3f206801
4tel <kyako95@gmail.com>
2024-11-20 14:25:39 +0900

    edit run.sh

--- xcrysden-1.6.2/run.sh
+++ xcrysden-1.6.2/run.sh
@@ -3,4 +3,8 @@
 #make clean
 make -j tcl tk mesa togl fftw
 make -j usage bwidget bindir
-make src-C src-F srcTcl
+make src-C src-F src-Tcl
+
+mkdir "${HOME}/bin" -p
+ln -s "${PWD}/xcrysden" "${HOME}/bin/xc"
+ln -s "${PWD}/xcrysden" "${HOME}/bin/"
